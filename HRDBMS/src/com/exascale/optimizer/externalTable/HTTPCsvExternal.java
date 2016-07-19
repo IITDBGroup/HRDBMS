d0f9da3660fd24a296216f75e35b81215f134fb9
Boris Glavic <lordpretzel@gmail.com>
2016-07-19 13:13:22 -0500

    ET interface

--- HRDBMS/src/com/exascale/optimizer/externalTable/HTTPCsvExternal.java
+++ HRDBMS/src/com/exascale/optimizer/externalTable/HTTPCsvExternal.java
@@ -4,59 +4,51 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Properties;
 import java.util.TreeMap;
 import com.exascale.optimizer.MetaData;
 import com.exascale.tables.Plan;
 
-public interface HTTPCsvExternal
+public class HTTPCsvExternal implements ExternalTableType
 {
-	public void add(HTTPCsvExternal httpCsvExtrnl) throws Exception;
 
-	public ArrayList<HTTPCsvExternal> children();
+	public static String HTTP_ADDRESS_KEY = "HttpAddress";
+	
+	// fields 
+	private int pos;
+	private List<String[]> rows;
+	
+	//chache
+	
+	@Override
+	public void initialize(Properties params)
+	{
+		rows = new ArrayList<String[]> ();
+		// TODO get HTTP Address
+		// TODO read CSV file content and cache in rows field
+	}
 
-	public HTTPCsvExternal clone();
+	@Override
+	public String[] next()
+	{
+		// TODO return rows[pos] and pos++ (check that still tuples avialable)
+		return null;
+	}
 
-	public void close() throws Exception;
+	@Override
+	public void reset()
+	{
+		// TODO pos = 0
+		
+	}
 
-	public int getChildPos();
+	@Override
+	public boolean hasNext()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 
-	public HashMap<String, Integer> getCols2Pos();
-
-	public HashMap<String, String> getCols2Types();
-
-	public MetaData getMeta();
-
-	public int getNode();
-
-	public TreeMap<Integer, String> getPos2Col();
-
-	public ArrayList<String> getReferences();
-
-	public Object next(HTTPCsvExternal httpCsvExtrnl) throws Exception;
-
-	public void nextAll(HTTPCsvExternal httpCsvExtrnl) throws Exception;
-
-	public long numRecsReceived();
-
-	public HTTPCsvExternal parent();
-
-	public boolean receivedDEM();
-
-	public void registerParent(HTTPCsvExternal httpCsvExtrnl) throws Exception;
-
-	public void removeChild(HTTPCsvExternal httpCsvExtrnl);
-
-	public void removeParent(HTTPCsvExternal httpCsvExtrnl);
-
-	public void reset() throws Exception;
-
-	public void serialize(OutputStream out, IdentityHashMap<Object, Long> prev) throws Exception;
-
-	public void setChildPos(int pos);
-
-	public void setNode(int node);
-
-	public void setPlan(Plan p);
-
-	public void start() throws Exception;
 }
