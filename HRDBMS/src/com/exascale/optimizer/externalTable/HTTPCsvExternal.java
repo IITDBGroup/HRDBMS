dce72d721f1a8d51b23cadce36f44b8b9bd62bf4
Jean Lucas Monte Carvalho <jean.rv@hotmail.com>
2016-07-26 16:03:48 -0500

    HTTPCsvExternal parsing working
    Parser for csv files found in a http address is working.

--- HRDBMS/src/com/exascale/optimizer/externalTable/HTTPCsvExternal.java
+++ HRDBMS/src/com/exascale/optimizer/externalTable/HTTPCsvExternal.java
@@ -1,54 +1,93 @@
 package com.exascale.optimizer.externalTable;
 
-import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Properties;
-import java.util.TreeMap;
-import com.exascale.optimizer.MetaData;
-import com.exascale.tables.Plan;
 
 public class HTTPCsvExternal implements ExternalTableType
 {
 
 	public static String HTTP_ADDRESS_KEY = "HttpAddress";
-	
-	// fields 
+
+	// fields
 	private int pos;
+	//This list holds a list of rows that are found in the initialize method
 	private List<String[]> rows;
-	
-	//chache
-	
+
 	@Override
 	public void initialize(Properties params)
 	{
-		rows = new ArrayList<String[]> ();
-		// TODO get HTTP Address
-		// TODO read CSV file content and cache in rows field
+		// key to find the http address in the params object
+		String HTTP_ADDRESS = params.getProperty(HTTP_ADDRESS_KEY);
+		// list of rows found in the csv file in the http address that should be returned to the rows list.
+		List<String[]> rowsToReturn = new ArrayList<String[]>();
+
+		try
+		{
+			// URL csvFile = new
+			// URL("http://hrdbms.esy.es/testfiles/products.csv");
+			URL csvFile = new URL(HTTP_ADDRESS);
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(csvFile.openStream()));
+			String inputLine;
+
+			String separator = ",";
+
+			/*
+			 * This is the parser spliting the data that it found into columns
+			 * of a map .
+			 */
+			while ((inputLine = in.readLine()) != null)
+			{
+				String[] output = inputLine.split(separator);
+				rowsToReturn.add(output);
+			}
+			in.close();
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+
+		rows = rowsToReturn;
 	}
 
 	@Override
 	public String[] next()
 	{
-		// TODO return rows[pos] and pos++ (check that still tuples avialable)
-		return null;
+		if (rows.get(pos) != null)
+		{
+			return rows.get(pos);
+		}
+		else
+		{
+			return null;
+		}
+
 	}
 
 	@Override
 	public void reset()
 	{
-		// TODO pos = 0
-		
+		pos = 0;
+
 	}
 
 	@Override
 	public boolean hasNext()
 	{
-		// TODO Auto-generated method stub
-		return false;
+		if (rows.get(pos + 1) != null)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
 	}
-	
 
 }
