86fb583dbd01a5763f460359d3ab231d2fb67921
michael <mykhaylo.kuzmyn@yahoo.com>
2017-02-18 10:55:25 -0600

    increased memory allocation for JVM in hparms updated hrdbms_start.sh reverted change in Dockerfile - based image should be pull from iitdbgroup

--- HRDBMS/docker/DockerCompose/debug/hrdbms_start.sh
+++ HRDBMS/docker/DockerCompose/debug/hrdbms_start.sh
@@ -1,3 +1,3 @@
 docker-compose down
 docker-compose up -d
-docker exec --user hrdbms hrdbms_coordinator nohup java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5010 -XX:+UseG1GC -XX:G1HeapRegionSize=2m -XX:+ParallelRefProcEnabled -XX:MaxDirectMemorySize=27772160000 -XX:+AggressiveOpts -cp /home/hrdbms/app/build/HRDBMS.jar: com.exascale.managers.HRDBMSWorker 0 &
+docker exec --user hrdbms hrdbms_coordinator java -cp /home/hrdbms/app/build/HRDBMS.jar: StartDB
