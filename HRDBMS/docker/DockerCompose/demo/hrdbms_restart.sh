3d011bf53c23e700b58df3fa243c9004896062e4
Boris Glavic <lordpretzel@gmail.com>
2017-02-21 14:17:05 -0600

    docker-compose for starting demo deployment of HRDBMS based on latest merged branch

---
+++ HRDBMS/docker/DockerCompose/demo/hrdbms_restart.sh
@@ -0,0 +1,3 @@
+docker exec --user hrdbms hrdbms_coordinator pkill -f java
+docker exec --user hrdbms hrdbms_worker pkill -f java
+docker exec --user hrdbms hrdbms_coordinator java -cp /home/hrdbms/app/bin: StartDB
