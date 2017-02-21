2cca60d24d5507fd9d61be4289820c510ee86572
Boris Glavic <lordpretzel@gmail.com>
2017-02-21 09:53:27 -0600

    modified travis build to download new ant

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -6,4 +6,4 @@ wget -q http://archive.apache.org/dist/ant/binaries/apache-ant-${ANT_VERSION}-bi
 tar -xzf apache-ant-${ANT_VERSION}-bin.tar.gz
 sudo mv apache-ant-${ANT_VERSION}/ /usr/share/ant
 sudo rm apache-ant-${ANT_VERSION}-bin.tar.gz
-sudo ln -s /usr/share/ant/apache-ant-${ANT_VERSION}/bin/ant /usr/bin/ant
+sudo ln -s /usr/share/ant/apache-ant-${ANT_VERSION}/bin/ant /usr/local/bin/ant
