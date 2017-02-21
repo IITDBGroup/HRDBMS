8fcd3895806e2234a0414f843520cc27267d0b13
Boris Glavic <lordpretzel@gmail.com>
2017-02-21 15:57:55 -0600

    client

--- HRDBMS/docker/DockerCompose/debug/hrdbms_client.sh
+++ HRDBMS/docker/DockerCompose/debug/hrdbms_client.sh
@@ -1,1 +1,1 @@
-docker exec --user hrdbms -ti hrdbms_coordinator java -cp /home/hrdbms/HRDBMS.jar:. com.exascale.cli.CLI
+docker exec --user hrdbms -ti hrdbms_coordinator java -cp /home/hrdbms/app/build/HRDBMS.jar:. com.exascale.cli.CLI
