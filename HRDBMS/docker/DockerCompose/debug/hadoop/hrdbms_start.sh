e1106dd2d167d2e2c09a9d89bffa60943b4b950f
Frank Morgan <fmorgan@hawk.iit.edu>
2017-04-23 14:14:52 -0400

    reverting docker start to prior - committed by accident

--- HRDBMS/docker/DockerCompose/debug/hadoop/hrdbms_start.sh
+++ HRDBMS/docker/DockerCompose/debug/hadoop/hrdbms_start.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-sudo docker-compose -f ./../docker-compose.yml -f ./docker-compose.yml down
+docker-compose -f ./../docker-compose.yml -f ./docker-compose.yml down
 # merging two compose yml files: fist one sets up hrdbms, second - hadoop
-sudo docker-compose -f ./../docker-compose.yml -f ./docker-compose.yml up -d
+docker-compose -f ./../docker-compose.yml -f ./docker-compose.yml up -d
 
 #start hrdbms
-sudo docker exec --user hrdbms hrdbmscoordinator java -cp /home/hrdbms/app/build/HRDBMS.jar: StartDB
+docker exec --user hrdbms hrdbmscoordinator java -cp /home/hrdbms/app/build/HRDBMS.jar: StartDB
 
 # wait until hadoop is launched
 sleep 10
 # first command fails for some reasons
-sudo docker exec hadoop /usr/local/hadoop/bin/hadoop fs -Ddfs.block.size=1048576 -put /tmp/csv_type.csv csv_type.csv
+docker exec hadoop /usr/local/hadoop/bin/hadoop fs -Ddfs.block.size=1048576 -put /tmp/csv_type.csv csv_type.csv
 # second command should work
 echo Copy csv file to HDFS
-sudo docker exec hadoop /usr/local/hadoop/bin/hadoop fs -Ddfs.block.size=1048576 -put /tmp/csv_type.csv csv_type.csv
+docker exec hadoop /usr/local/hadoop/bin/hadoop fs -Ddfs.block.size=1048576 -put /tmp/csv_type.csv csv_type.csv
 
 echo Copy large csv file to HDFS
-sudo docker exec hadoop /usr/local/hadoop/bin/hadoop fs -Ddfs.block.size=1048576 -put /tmp/file.csv file.csv
+docker exec hadoop /usr/local/hadoop/bin/hadoop fs -Ddfs.block.size=1048576 -put /tmp/file.csv file.csv
